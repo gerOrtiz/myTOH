0da149d701ff4a08dbc55d4f266fb140e7151d9e
Gerson Ortiz <ger.ortiz7@gmail.com>
2017-04-10 12:23:36 -0500

    Revert "first commit"
    This reverts commit 49aa586be983f0e341f0e39338fcb3b3db277c3b.
    
    # Conflicts:
    #	README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
 export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 5, name: 'Batman' },
+  { id: 6, name: 'Wolverine' },
+  { id: 7, name: 'Flash' },
+  { id: 8, name: 'Thor' },
+  { id: 9, name: 'Ant-Man' },
+  { id: 10, name: 'Rogue' },
+  { id: 11, name: 'Gambit' },
+  { id: 12, name: 'Deadpool' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Scarlet Witch' },
+  { id: 15, name: 'CatWoman' }
+];
